0cfa694da4e2c60c51c2effd99d3137e14a0671b
Raju Islam <rajuislam.dev@gmail.com>
2023-09-25 01:04:04 +0600

    Your commit message

---
+++ send_mail.php
@@ -0,0 +1,34 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $to = "marsfarhad@gmail.com"; // Replace with the recipient's email address
+    $headers = "From: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Gmail SMTP configuration
+    $smtpHost = "smtp.gmail.com";
+    $smtpPort = 587;
+    $smtpUsername = "marsfarhad@gmail.com";
+    $smtpPassword = "Moheshpur01##";
+
+    // Create the email message
+    $emailMessage = "Name: $name\n";
+    $emailMessage .= "Email: $email\n";
+    $emailMessage .= "Subject: $subject\n";
+    $emailMessage .= "Message:\n$message";
+
+    // Send the email using Gmail SMTP
+    if (mail($to, $subject, $emailMessage, $headers)) {
+        header("Location: success.html"); // Redirect to a success page
+    } else {
+        header("Location: error.html"); // Redirect to an error page
+    }
+} else {
+    // If someone tries to access this script directly, redirect them to the form page
+    header("Location: contact.html");
+}
+?>
